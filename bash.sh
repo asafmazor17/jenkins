1d33fbaeba053f967cb3cf8708aa875aa23ca07a
asafmazor17 <77241512+asafmazor17@users.noreply.github.com>
2021-05-02 14:59:46 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,13 +3,13 @@
 s1="2"
 s2="4"
 
-if [ $s1 == $s2 ]
+if [ "$s1" == "$s2" ]
 then
 echo "They are equal!"
-elif [ $s1 > $s2 ]
+elif [ "$s1" > "$s2" ]
 then
 echo " $s1 is biggger than $s2"
-elif [ $s2 > $s1 ]
+elif [ "$s2" > "$s1" ]
 then 
 echo " $s2 is bigger than $s1"
 else 
